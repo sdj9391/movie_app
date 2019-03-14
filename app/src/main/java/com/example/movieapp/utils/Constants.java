29961ed40e282807e02fc9166534da6070392af5
sj <sumit.j@bootinfotech.com>
2019-03-15 00:34:32 +0530

    Removed API key

--- app/src/main/java/com/example/movieapp/utils/Constants.java
+++ app/src/main/java/com/example/movieapp/utils/Constants.java
@@ -1,7 +1,7 @@
 package com.example.movieapp.utils;
 
 public class Constants {
-    public static String MOVIE_API_KEY = "8d6039726b8eefeb9e2c14756baf8018";
+    public static String MOVIE_API_KEY = "api key";
     public static String IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
     public static String BASE_URL = "https://api.themoviedb.org/3/";
     public static String LANGUAGE = "en-US";
